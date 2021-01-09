6f05405bc817747dd8ca224a512bcd338beab874
erastus <erastuskariuki15@gmail.com>
2021-01-09 10:32:17 +0300

    added save function on images

--- photos/tests.py
+++ photos/tests.py
@@ -6,4 +6,16 @@ class ImageTestClass(TestCase):
 
     # Set up method
     def setUp(self):
-        self.image= Image(title = 'smoothie', category ='self.category', description ='best smoothie in nairobi area and surrounding', location = 'nairobi', image_url = 'https://moringaschool.instructure.com/courses/329/pages/tuesday-testing-django-apps?module_item_id=2449' )
+        self.image= Image(title = 'smoothie', category ='category', description ='best smoothie in nairobi area and surrounding', location = 'nairobi', image_url = 'https://moringaschool.instructure.com/courses/329/pages/tuesday-testing-django-apps?module_item_id=2449' )
+
+    
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
