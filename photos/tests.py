dd0ceb412575f73dbf236df4be6209e7673cee11
erastus <erastuskariuki15@gmail.com>
2021-01-09 10:15:21 +0300

    added test function of image

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Image,Location,Category
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.image= Image(title = 'smoothie', category ='self.category', description ='best smoothie in nairobi area and surrounding', location = 'nairobi', image_url = 'https://moringaschool.instructure.com/courses/329/pages/tuesday-testing-django-apps?module_item_id=2449' )
