ae5fc64b16db73d5b2c1d2eac951f9de01940087
erastus <erastuskariuki15@gmail.com>
2021-01-09 12:36:52 +0300

    added models in the admin.py

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Image,Category,Location
 
-# Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal =('category',)
+
+admin.site.register(Image,ImageAdmin)
+admin.site.register(Category)
+admin.site.register(Location)
