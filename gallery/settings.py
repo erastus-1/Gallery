d237ade69f4124bd3d54430219ae2b6c0539e8cb
erastus <erastuskariuki15@gmail.com>
2021-01-09 08:29:36 +0300

    installed and added cloudinary

--- gallery/settings.py
+++ gallery/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary'
 ]
+
+cloudinary.config( 
+  cloud_name = config('cloud_name'),
+  api_key = config('api_key'),
+  api_secret = config('api_secret'), 
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -118,3 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
