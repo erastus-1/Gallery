387b15bb2ba9febd78206c8f2a5b71e4ba3a3a46
erastus <erastuskariuki15@gmail.com>
2021-01-09 15:34:10 +0300

    updated views

--- photos/views.py
+++ photos/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Image
+from django.db import models
 import datetime as dt
 
 # Create your views here.
 def home(request):
-    return render(request, 'all/home.html')
+    title = 'Welcome to Photo Appy'
+    images = Image.objects.all()
+    return render(request, 'all/home.html',{"title" :title, "images":images})
 
 def about(request):
-    return render(request, 'all/about.html')
+    title = 'About'
+    return render(request, 'all/about.html', {"title" :title})
 
 def search_results(request):
 
