93d72a4b51dd74e4e420c6e8b6ac09aee22fc539
erastus <erastuskariuki15@gmail.com>
2021-01-09 13:28:23 +0300

    added search view

--- photos/views.py
+++ photos/views.py
@@ -5,3 +5,16 @@ import datetime as dt
 # Create your views here.
 def home(request):
     return render(request, 'all/home.html')
+
+def search_results(request):
+
+    if 'images' in request.GET and request.GET["images"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all/search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all/search.html',{"message":message})
