baf5686f1c6711e4a9f1e9b30da9962c2fcf0c12
erastus <erastuskariuki15@gmail.com>
2021-01-09 13:50:13 +0300

    update phoots urls

--- photos/urls.py
+++ photos/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     url('^$',views.home,name = 'home'),
+    url('^$',views.about,name = 'about'),
     url(r'^search/', views.search_results, name='search_results')
 ]
