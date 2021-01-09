8212964622e03e5531dc88f6d6a87fdd354d4dbb
erastus <erastuskariuki15@gmail.com>
2021-01-09 13:16:19 +0300

    added search model

--- photos/models.py
+++ photos/models.py
@@ -11,15 +11,17 @@ class Image(models.Model):
     image_url = CloudinaryField('image', blank=True)   
     date_uploaded = models.DateTimeField(auto_now_add=True)
 
-
     def __str__(self):
         return self.first_name
 
     class Meta:
         ordering = ['date_uploaded']
 
-    def save_image(self):
-        self.save()
+    @classmethod
+    def search_by_title(cls,search_term):
+        news = cls.objects.filter(title__icontains=search_term)
+        return news
+
 
 class Category(models.Model):
     category = models.CharField(max_length =30)
@@ -33,6 +35,7 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category
+        
 
 class Location(models.Model):
     name = models.CharField(max_length =30)
