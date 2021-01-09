63a14a4cfc9af8ff5bab4583da74eaf8583b224b
erastus <erastuskariuki15@gmail.com>
2021-01-09 09:26:59 +0300

    added html link

--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-
+import datetime as dt
+from django.shortcuts import render
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse("Welcome to the Ruiru's Gallery")
+    return render(request, 'welcome.html')
